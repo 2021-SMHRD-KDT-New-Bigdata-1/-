1292a51ad8ff107d41cc1f14931fad19faef1b88
smhrd <smhrd@DESKTOP-97MULPS>
2021-12-01 07:11:43 +0900

    Mypage, Test 도메인(mypage1에서 필요한 vo)작성

---
+++ project/src/main/java/kr/hong/domain/Mypage.java
@@ -0,0 +1,13 @@
+package kr.hong.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Mypage {
+		private String study_date;
+		private float accuracy;
+}
---
+++ project/src/main/java/kr/hong/domain/Test.java
@@ -0,0 +1,18 @@
+package kr.hong.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Test {
+		private int num;
+		private String content;
+		private String study_date;
+		private String reg_date;
+		private float accuracy;
+	
+}
